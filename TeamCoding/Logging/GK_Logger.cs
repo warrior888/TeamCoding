3419162494620ea3b2257176fa5e87118ab9a7b1
GKASZTELANIEC7N\GKasztelaniec <zombi3s@gmail.com>
2017-05-25 19:11:03 +0200

    Added write log to file in windows myDocuments and test module for checking local IP number and logged windows user executing curent project in VS.

---
+++ TeamCoding/Logging/GK_Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TeamCoding.Logging
+{
+    public class GK_Logger
+    {
+
+        public void WriteToFile()
+        {
+            System.Diagnostics.Debug.WriteLine("Windows user name: "+System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+            IPHostEntry host;
+            string localIp;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIp = ip.ToString();
+                    System.Diagnostics.Debug.WriteLine("My local ip: "+localIp);
+                }
+            }
+
+            /*
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile) = new StreamWriter(mydocpath + @"\TeamCodingLog.log"))
+            {
+                outputFile.WriteLine("error");
+            }
+            */
+
+        }
+    }
+}
