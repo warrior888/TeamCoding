bd0a26d2043d83573a5e2fca07fab5d4cf2042ed
Erley <kopczykf@gmail.com>
2017-06-17 15:38:33 +0200

    Add New Item

--- TeamCoding/VisualStudio/Models/LocalIDEModel.cs
+++ TeamCoding/VisualStudio/Models/LocalIDEModel.cs
@@ -209,6 +209,8 @@ namespace TeamCoding.VisualStudio.Models
 
             //ProjectManager.Dte.ExecuteCommand("Project.AddNewItem", @"Z:\Projects\TeamCodingWarrior\TeamCoding.Tests\TeamCoding.Tests\test.txt");
             //ProjectManager.Dte.ExecuteCommand("File.NewFile");
+            //ProjectManager.Dte.ItemOperations.AddNewItem("Code\\class","klasa.cs");
+
 
             //    Process process = new Process();
             
