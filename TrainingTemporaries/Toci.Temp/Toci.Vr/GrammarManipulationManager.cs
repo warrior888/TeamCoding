573766ed134b5a7a421ed6f9efae45a991c69237
warrior888 <warriorr@poczta.fm>
2017-06-18 09:56:54 +0200

    vr homework

---
+++ TrainingTemporaries/Toci.Temp/Toci.Vr/GrammarManipulationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace Toci.Vr
+{
+    public class GrammarManipulationManager
+    {
+        private string grammarPath;
+        public GrammarManipulationManager(string path)
+        {
+            grammarPath = path;
+        }
+        public void AddChoiceToGrammar(string ruleId, string item)
+        {
+            XDocument document = XDocument.Load(grammarPath);
+
+
+            //document.Nodes()
+            
+        }
+    }
+}
