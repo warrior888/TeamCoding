26a63d1f3998c9b0adb95ded5d6cfbeb76810d79
Erley <kopczykf@gmail.com>
2017-06-21 14:23:52 +0200

    Fix Plugin Form

--- TeamCoding/TeamCodingToolbarSCMConnectButton.cs
+++ TeamCoding/TeamCodingToolbarSCMConnectButton.cs
@@ -21,20 +21,14 @@ namespace TeamCoding
     /// </summary>
     internal sealed class TeamCodingToolbarSCMConnectButton
     {
-        public Button ListeningButton = new Button();
-        public Button ConnectButton = new Button();
-        public Button DisconnectButton = new Button();
-	    public new Button CancelButton = new Button();
-        private VrCommandsManager vrCommandsManager;
-        private const string FormName = "TOCI TeamCoding";
-	    public Form _form;
+	    public VrPluginForm PluginForm;
 
         /// <summary>
         /// Command ID.
         /// </summary>
         public const int CommandId = 256;
 
-        /// <summary>
+	    /// <summary>
         /// Command menu group (command set GUID).
         /// </summary>
         public static readonly Guid CommandSet = new Guid("62aae97c-b7dd-41ba-bc5d-6d2901aa3852");
@@ -105,70 +99,13 @@ namespace TeamCoding
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-	        _form = new Form();
-			var instruction = new DevHandledInstruction(){FileType = "interface",FileName = "oleoleole", FileContent = ""};
-            var form = new VrAddNewItemForm(instruction);
-            form.Show();
-
-            ConnectButton.Click += ConnectButton_Click;
-            ListeningButton.Click += ListeningButton_Click;
-            CancelButton.Click += CancelButton_Click;
-            DisconnectButton.Click += DisconnectButton_Click;  
-
-            ConnectButton.Size = new Size(80,25);
-            ConnectButton.ClientSize = ConnectButton.Size;
-            ConnectButton.Location = new Point(10,10);
-            ConnectButton.Text = "Connect";
-
-            ListeningButton.Size = new Size(80,25);
-            ListeningButton.ClientSize = ListeningButton.Size;
-            ListeningButton.Location = new Point(100,10);
-            ListeningButton.Text = "Listening";
-            
-            DisconnectButton.Size = new Size(80,25);
-            DisconnectButton.ClientSize = DisconnectButton.Size;
-            DisconnectButton.Location = new Point(190,10);
-            DisconnectButton.Text = "Disconnect";
-
-            CancelButton.Size = new Size(80,25);
-            CancelButton.ClientSize = CancelButton.Size;
-            CancelButton.Location = new Point(100,40);
-            CancelButton.Text = "Cancel";
-            
-            _form.Text = FormName;
-			_form.Size = new Size(282,72);
-			_form.StartPosition = FormStartPosition.CenterScreen;
-			_form.ClientSize = _form.Size;
-			_form.Controls.Add(ConnectButton);
-			_form.Controls.Add(ListeningButton);
-			_form.Controls.Add(CancelButton);
-			_form.Controls.Add(DisconnectButton);
-
-			_form.Show();
+	        PluginForm = new VrPluginForm();
+			//var instruction = new DevHandledInstruction(){FileType = "interface",FileName = "oleoleole", FileContent = ""};
+            //var form = new VrAddNewItemForm(instruction);
+            //form.Show();
+	        PluginForm.Show();
         }
 
-        private void DisconnectButton_Click(object sender, EventArgs e)
-        {
-            BroadcastManager.StopSCMClient();
-			_form.Close();
-        }
-
-        private void CancelButton_Click(object sender, EventArgs e)
-        {
-			_form.Close();
-        }
-
-        private void ListeningButton_Click(object sender, EventArgs e)
-        {
-            vrCommandsManager = new VrCommandsManager();
-            vrCommandsManager.Register();
-			_form.Close();
-        }
-
-        private void ConnectButton_Click(object sender, EventArgs e)
-        {
-            BroadcastManager.StartSCMClient();
-			_form.Close();
-        }
+        
     }
 }
---
+++ TeamCoding/Toci/Implementations/VrPluginForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TeamCoding.Toci.Implementations
+{
+	public class VrPluginForm : Form
+	{
+		public Button ListeningButton = new Button();
+		public Button ConnectButton = new Button();
+		public Button DisconnectButton = new Button();
+		public new Button CancelButton = new Button();
+		private const string FormName = "TOCI TeamCoding";
+		private VrCommandsManager vrCommandsManager;
+
+
+		public VrPluginForm()
+		{
+			ConnectButton.Click += ConnectButton_Click;
+			ListeningButton.Click += ListeningButton_Click;
+			CancelButton.Click += CancelButton_Click;
+			DisconnectButton.Click += DisconnectButton_Click;
+
+			ConnectButton.Size = new Size(80, 25);
+			ConnectButton.ClientSize = ConnectButton.Size;
+			ConnectButton.Location = new Point(10, 10);
+			ConnectButton.Text = "Connect";
+
+			ListeningButton.Size = new Size(80, 25);
+			ListeningButton.ClientSize = ListeningButton.Size;
+			ListeningButton.Location = new Point(100, 10);
+			ListeningButton.Text = "Listening";
+
+			DisconnectButton.Size = new Size(80, 25);
+			DisconnectButton.ClientSize = DisconnectButton.Size;
+			DisconnectButton.Location = new Point(190, 10);
+			DisconnectButton.Text = "Disconnect";
+
+			CancelButton.Size = new Size(80, 25);
+			CancelButton.ClientSize = CancelButton.Size;
+			CancelButton.Location = new Point(100, 40);
+			CancelButton.Text = "Cancel";
+
+			this.Text = FormName;
+			this.Size = new Size(282, 72);
+			this.StartPosition = FormStartPosition.CenterScreen;
+			this.ClientSize = this.Size;
+			this.Controls.Add(ConnectButton);
+			this.Controls.Add(ListeningButton);
+			this.Controls.Add(CancelButton);
+			this.Controls.Add(DisconnectButton);
+
+		}
+
+		private void DisconnectButton_Click(object sender, EventArgs e)
+		{
+			BroadcastManager.StopSCMClient();
+			this.Close();
+		}
+
+		private void CancelButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void ListeningButton_Click(object sender, EventArgs e)
+		{
+			vrCommandsManager = new VrCommandsManager();
+			vrCommandsManager.Register();
+			this.Close();
+		}
+
+		private void ConnectButton_Click(object sender, EventArgs e)
+		{
+			BroadcastManager.StartSCMClient();
+			this.Close();
+		}
+	}
+}
