8d5e6580feeadf1997a2f1f91f78bb8384b3ef4e
warrior888 <warriorr@poczta.fm>
2017-10-01 09:34:41 +0200

    tr start

---
+++ TrainingTemporaries/Toci.Hack.Tc.Traininig.Reflection/Toci.Hack.Tc.Traininig.Reflection/ReflectionExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Hack.Tc.Traininig.Reflection
+{
+    public class ReflectionExample
+    {
+    }
+}
